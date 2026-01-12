af16c7d0429fe18336a98f14d5d1ef7e6a7ee9b3
lixiang <lixiang14@wps.cn>
2026-01-12 15:51:20 +0800

    fix: 更新页面背景颜色
    页面背景颜色不正确，应该设置为#fdfdfd 仓库地址： https://github.com/JonathanLee-LX/vue3-app.git
    
    页面背景颜色不正确的问题是由于没有在根组件App.vue中设置全局背景颜色。修复方案是在App.vue中添加一个全局样式块（非scoped），设置#app元素的背景颜色为#fdfdfd，并修改模板以包含一个id为'app'的根div元素。这样修改可以确保背景颜色应用到整个应用，而不会影响其他组件的局部样式，符合最小修改原则。

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,29 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
+
+<style>
+#app {
+  background-color: #fdfdfd;
+}
+</style>
 
 <style scoped>
 header {
